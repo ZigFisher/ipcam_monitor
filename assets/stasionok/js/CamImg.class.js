fd1ce4604c0e3fb4ecb442afdbd7f0feaee5bb39
kpuk <stasionok@gmail.com>
2018-11-09 19:03:01 +0300

    move js into class

---
+++ assets/stasionok/js/CamImg.class.js
@@ -0,0 +1,151 @@
+class CamImg {
+    constructor(camera, datepicker, fotorama) {
+        if (typeof CamImg.inst === 'undefined') CamImg.inst = {};
+
+        if (typeof CamImg.inst[camera] !== 'undefined' && typeof CamImg.inst[camera]) return CamImg.inst[camera];
+
+        CamImg.inst[camera] = this;
+        this.imagesList = [];
+        this.baseUrl = '/~rewm/' + camera + '/';
+        this.startIndex = 1;
+        this.shiftSlides = 80;
+        this.refreshTimeout = 60;
+        this.datepicker = datepicker;
+        this.fotorama = fotorama;
+
+        this.initHour = 0;
+        this.initMinute = 0;
+        return this;
+    }
+
+    getFullUrl() {
+        let date = this.getDateTime();
+        let url = date.getFullYear() + '/' + this.getLeadingZeroNum(date.getMonth() + 1) + '/' + this.getLeadingZeroNum(date.getDate());
+        return this.baseUrl + url;
+    }
+
+    getHtml() {
+        return new Promise((resolve, reject) => {
+            let url = this.getFullUrl();
+            url = 'http://localhost:63343/cam-img-to-video/example.html'; // FIXME: DEBUG!!
+
+            $.get(url, function (data) {
+                if (!data.length) return reject('Can`t get filelists');
+                let html = $.parseHTML(data);
+                return resolve(html);
+            });
+        });
+    }
+
+    getImagesList() {
+        return this.getHtml()
+            .then((html) => {
+                this.imagesList = $(html).find("a").toArray();
+                return this.imagesList;
+            });
+
+
+    }
+
+    sliceImages(images) {
+        var total = Object.entries(images).length,
+            indx = this.startIndex,
+            min = indx - this.shiftSlides,
+            max = indx + this.shiftSlides;
+
+        if (min < 0) min = 0;
+        if (max > total) max = total;
+
+        var result = Object.entries(images).slice(min, max).map(entry => entry[1]);
+
+        this.startIndex = this.shiftSlides;
+        if (min === 0) this.startIndex = indx;
+        if (max === total) this.startIndex = result.length - (total - indx);
+
+        return result;
+    }
+
+    setDatepicker(datepicker) {
+        this.datepicker = datepicker;
+    }
+
+    setFotorama(fotorama) {
+        this.fotorama = fotorama;
+    }
+
+    getPictures() {
+        return new Promise((resolve, reject) => {
+            this.getInitTime();
+            this.getImagesList()
+                .then(() => {
+                    let images = {},
+                        fullUrl = this.getFullUrl(),
+                        _this = this;
+
+                    this.imagesList.forEach(function (el, i) {
+                        if (i > 0) {
+                            let href = $(el).attr('href');
+
+                            // set start index
+                            let hour = href.replace('.jpg', '').split('.');
+                            let minute = +hour[1];
+                            hour = +hour[0];
+                            if (hour <= _this.initHour && minute <= _this.initMinute) {
+                                _this.startIndex = i - 1;
+                            }
+
+                            images[i] = {
+                                img: fullUrl + '/' + href,
+                                caption: _this.getLeadingZeroNum(hour) + ':' + _this.getLeadingZeroNum(minute),
+                            };
+                        }
+                    });
+
+                    return resolve(images);
+                });
+        });
+    }
+
+    showPictures() {
+        this.getPictures()
+            .then(images => {
+                let sliced = this.sliceImages(images);
+                if (sliced.length) {
+                    this.fotorama
+                        .load(sliced)
+                        .show(this.startIndex);
+                }
+            });
+    }
+
+    setRefresh(refresh) {
+        if (!refresh) {
+            if (this.refresh) {
+                clearInterval(this.refresh);
+            }
+            return false;
+        }
+
+        let _this = this;
+
+        this.refresh = setInterval(function () {
+            let current = _this.getDateTime();
+            _this.datepicker.selectDate(new Date(current.getTime() + _this.refreshTimeout * 1000));
+            return _this.showPictures();
+        }, this.refreshTimeout);
+    }
+
+    getInitTime() {
+        let dt = this.getDateTime();
+        this.initHour = +dt.getHours();
+        this.initMinute = +dt.getMinutes();
+    }
+
+    getLeadingZeroNum(num) {
+        return parseInt(num) < 10 ? '0' + num : num;
+    }
+
+    getDateTime() {
+        return new Date(this.datepicker.selectedDates);
+    }
+}
--- assets/stasionok/js/main.js
+++ assets/stasionok/js/main.js
@@ -1,12 +1,39 @@
-var urlParams = new URLSearchParams(window.location.search),
-    camera = urlParams.get('camera'),
-    refresh = urlParams.get('refresh'),
-    refreshInterval = false,
-    lastDate = false,
-//  baseUrl = 'http://ipcam.octonix.net/~rewm/' + camera + '/',
-    baseUrl = '/~rewm/' + camera + '/',
-    shiftSlides = 80,
-    refreshTimeout = 60;
+$(function () {
+    var urlParams = new URLSearchParams(window.location.search),
+        camera = urlParams.get('camera'),
+        refresh = urlParams.get('refresh'),
+        datepicker = $('#picker').data('datepicker'),
+        fotorama = $('#fotorama').fotorama().data('fotorama');
+
+    if (camera !== null) {
+        $('#onecam').show();
+        $('#camblock').hide();
+        $('h2').html(camera);
+        datepicker.selectDate(new Date()); // set now as default datetime
+
+        let cameraClass = new CamImg(camera, datepicker, fotorama);
+
+        cameraClass.showPictures();
+        cameraClass.setRefresh(refresh); // if refresh = true set interval
+
+    } else {
+        $('#onecam').hide();
+        $('#camblock').show();
+        setLastCamsImg();
+        setInterval(setLastCamsImg, refreshTimeout * 1000);
+    }
+
+
+    $('#show').click(function () {
+        if (!datepicker.selectedDates.length) return false;
+        if (camera === null) return false;
+        let cameraClass = new CamImg(camera, datepicker, fotorama); //get exists with singleton
+
+        cameraClass.showPictures();
+    });
+
+});
+
 
 function setLastCamsImg() {
     $('.camlist-item').each(function (i, item) {
@@ -35,131 +62,8 @@ function setLastCamsImg() {
     })
 }
 
-$(function () {
-    var datepicker = $('#picker').data('datepicker'),
-        fotorama = $('#fotorama').fotorama().data('fotorama'),
-        images = {},
-        html = '',
-        lastDate = false;
-
-    $('#show').click(function () {
-        if (!datepicker.selectedDates.length) return false;
-        if (camera === null) return false;
-
-        var dt = new Date(datepicker.selectedDates),
-            date = dt.getFullYear() + '/' + getLeadingZeroNum(dt.getMonth() + 1) + '/' + getLeadingZeroNum(dt.getDate()),
-            url = baseUrl + date,
-            initHour = +dt.getHours(),
-            initMinute = +dt.getMinutes(),
-            startindex = 1;
-
-        if (lastDate !== date) {
-            // url = 'http://localhost:63343/cam-img-to-video/example.html';
-            $.get(url, function (data) {
-                html = $.parseHTML(data);
-
-                lastDate = date;
-                images = {};
-                $(html).find("a").each(function (i, el) {
-                    if (i > 0) {
-                        var href = $(el).attr('href');
-
-                        // set start index
-                        var hour = href.replace('.jpg', '').split('.');
-                        var minute = +hour[1];
-                        hour = +hour[0];
-                        if (hour <= initHour && minute <= initMinute) {
-                            startindex = i - 1;
-                        }
-
-                        images[i] = {
-                            img: baseUrl + date + '/' + href,
-                            caption: [getLeadingZeroNum(hour), getLeadingZeroNum(minute)].join(':'),
-                        };
-                    }
-                });
-                // delete images[Object.keys(images).length];
-                var toShow = sliceImages(images, startindex);
-
-                fotorama
-                    .load(toShow.images)
-                    .show(toShow.startfrom);
-            });
-        } else {
-            $(html).find("a").each(function (i, el) {
-                if (i > 0) {
-                    var href = $(el).attr('href');
-
-                    // set start index
-                    var hour = href.replace('.jpg', '').split('.');
-                    var minute = hour[1];
-                    hour = hour[0];
-                    if (hour <= initHour && minute <= initMinute) {
-                        startindex = i - 1;
-                    }
-                }
-            });
-
-            var toShow = sliceImages(images, startindex);
-            fotorama
-                .load(toShow.images)
-                .show(toShow.startfrom);
-        }
-    });
-
-    if (camera !== null) {
-        $('#onecam').show();
-        $('#camblock').hide();
-        $('h1').html(camera);
-        datepicker.selectDate(new Date());
-        $('#show').click();
-        var onLoad = true;
-        $('#fotorama').on('fotorama:load', function (e, fotorama) {
-            if (onLoad) {
-                fotorama.show(shiftSlides - 1);
-                onLoad = false;
-                if (refresh) {
-                    refreshInterval = setInterval(function () {
-                        setNextCamImg(datepicker)
-                    }, refreshTimeout * 1000);
-                }
-            }
-        });
-    } else {
-        $('#onecam').hide();
-        $('#camblock').show();
-        setLastCamsImg();
-        setInterval(setLastCamsImg, refreshTimeout * 1000);
-    }
-});
 
 function getLeadingZeroNum(num) {
     return parseInt(num) < 10 ? '0' + num : num;
 }
 
-function sliceImages(imgs, indx) {
-    var total = Object.entries(imgs).length,
-        min = indx - shiftSlides,
-        max = indx + shiftSlides;
-
-    if (min < 0) min = 0;
-    if (max > total) max = total;
-
-    var result = Object.entries(imgs).slice(min, max).map(entry => entry[1]);
-
-    startfrom = shiftSlides;
-    if (min === 0) startfrom = indx;
-    if (max === total) startfrom = result.length - (total - indx);
-
-    return {
-        images: result,
-        startfrom: startfrom
-    };
-}
-
-function setNextCamImg(datepicker) {
-    var current = new Date(datepicker.selectedDates);
-    datepicker.selectDate(new Date(current.getTime() + refreshTimeout * 1000));
-    lastDate = false;
-    $('#show').click();
-}
